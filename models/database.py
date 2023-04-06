64161061fee6f3dca6943e8c7e70fe9b2f15bda1
Sofagris <roy@stua.net>
2023-04-06 20:32:28 +0200

    Removed some comments

--- models/database.py
+++ models/database.py
@@ -1,6 +1,5 @@
 # models/database.py
 from sqlalchemy import create_engine
-#from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
--- models/user_models.py
+++ models/user_models.py
@@ -1,7 +1,6 @@
 # models/user_models.py
 
 from sqlalchemy import Column, Integer, String
-#from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 
 
