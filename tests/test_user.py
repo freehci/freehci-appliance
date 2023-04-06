253ed48342a753a34e6a1c5c72d0713141af9bd0
Sofagris <roy@stua.net>
2023-04-06 18:11:20 +0200

    Added some tests

---
+++ tests/test_user.py
@@ -0,0 +1,21 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app  # Import app-instance from main.py
+
+client = TestClient(app)
+
+def test_get_user_by_id():
+    # Create a testuser and add it to the DB
+    test_user_id = 3
+    # Test for a valid user-ID
+    response = client.get(f"/users/{test_user_id}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": test_user_id,
+        "username": "test_username",
+        "email": "test@example.com",
+    }
+
+    # Test for a invalid user-ID
+    response = client.get("/users/0")
+    assert response.status_code == 404
