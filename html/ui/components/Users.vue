42ac9556a4ae2cb06805f31818b85b7bfbc55adc
Sofagris <roy@stua.net>
2023-04-09 14:51:03 +0200

    Translations

---
+++ html/ui/components/Users.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h2>Users</h2>
+      <!-- Rest of the HTML-code for the Users-component -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Users',
+    data() {
+      return {
+        // Data for the Users-component
+      };
+    },
+    methods: {
+      // Methods for the Users-component
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* CSS for the Users-component */
+  </style>
+  
