62b17d74161dfe7605e53e32fbd7e9e2ba561e74
Sofagris <roy@stua.net>
2023-04-16 18:58:11 +0200

    Footer and styling

--- html/ui/components/Footer.vue
+++ html/ui/components/Footer.vue
@@ -1,41 +1,97 @@
 <template>
     <footer class="footer">
       <div class="footer-toggle" @click="toggleFooter">
-        <i class="fas fa-chevron-up" v-if="footerExpanded"></i>
-        <i class="fas fa-chevron-down" v-else></i>
+        <i class="fas fa-chevron-down" v-if="footerExpanded"></i>
+        <i class="fas fa-chevron-up" v-else></i>
         <span>Event Log</span>
       </div>
       <div class="footer-content" v-show="footerExpanded">
-        <div class="event-log">
-          <ul>
-            <li v-for="event in eventLog" :key="event.id">{{ event.timestamp }} - {{ event.message }}</li>
-          </ul>
+        <div class="event-log mt-2">
+            <div class="event-log-header">
+            <div class="row">
+                <div class="col-md-2">Time</div>
+                <div class="col-md-3">Task / Event</div>
+                <div class="col-md-2">Status</div>
+                <div class="col-md-2">Progress</div>
+                <div class="col-md-3">User</div>
+            </div>
+            </div>
+            <div class="event-log-content">
+            <ul class="list-group">
+                <li class="list-group-item" v-for="event in eventLog" :key="event.id">
+                    <div class="row">
+                        <div class="col-md-2">{{ event.time }}</div>
+                        <div class="col-md-3">{{ event.task }}</div>
+                        <div class="col-md-2">{{ event.status }}</div>
+                        <div class="col-md-2">{{ event.progress }}</div>
+                        <div class="col-md-3">{{ event.user }}</div>
+                    </div>
+                </li>
+            </ul>
+            </div>
         </div>
       </div>
     </footer>
-  </template>
+</template>
   
-  <script>
+  
+<script>
     export default {
         data() {
             return {
                 footerExpanded: false,
                 eventLog: [
-                { id: 1, timestamp: '2023-04-15 09:30:00', message: 'User logged in.' },
-                { id: 2, timestamp: '2023-04-15 10:15:00', message: 'User created a new post.' },
-                // Add more events as needed
+                    {
+                        id: 1,
+                        time: '09:30:00',
+                        task: 'Login',
+                        status: 'Success',
+                        progress: '100%',
+                        user: 'John Doe',
+                    },
+                    {
+                        id: 2,
+                        time: '10:15:00',
+                        task: 'Create post',
+                        status: 'Success',
+                        progress: '100%',
+                        user: 'John Doe',
+                    },
+                    // Add more events as needed
                 ],
             };
-            },
-            methods: {
+        },
+        methods: {
             toggleFooter() {
                 this.footerExpanded = !this.footerExpanded;
             },
         },
     };
-  </script>
+</script>
 
 <style>
+    
+    .event-log {
+        background-color: #f8f9fa;
+        border: 1px solid #dee2e6;
+        border-radius: 0.25rem;
+    }
 
+    .event-log-header {
+        background-color: #343a40;
+        color: #fff;
+        padding: 0.5rem;
+    }
+
+    .event-log-content {
+        max-height: 300px;
+        overflow-y: auto;
+    }
+
+    .list-group-item {
+       background-color: #f1f1f1;
+        color: #333;
+        border-bottom: 1px solid #ccc;
+    }
 
 </style>
