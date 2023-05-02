1cf656f01863dd0064833a4cd0041905560dbf27
Sofagris <roy@stua.net>
2023-05-02 12:31:10 +0200

    .gitignore changes

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # main.py
+
+# ANSI color codes
+RED = "\033[31m"
+GREEN = "\033[32m"
+BLUE = "\033[34m"
+YELLOW = "\033[33m"
+RESET = "\033[0m"
+
 print("Starting application...")
 import logging
 #from logging import SysLogHandler # This is not available on Windows
